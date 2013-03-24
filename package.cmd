628f636d36b98cb1cc518b3d0fbfa68d45bea949
Nick Aceves <nickaceves@gmail.com>
2013-03-24 02:23:07 -0500

    The nuget package now works the way I want.  Woot.

--- package.cmd
+++ package.cmd
@@ -5,6 +5,6 @@ del /Q /S build\*
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild /v:n /p:Configuration=Release ..\Nmpq\Nmpq.csproj
 ILMerge.exe /out:build\Nmpq.dll /t:library ..\Nmpq\bin\Release\Nmpq.dll ..\Nmpq\bin\Release\ICSharpCode.SharpZipLib.dll
 
-
+nuget pack Nmpq.dll.nuspec
 
 popd
